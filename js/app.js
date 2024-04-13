45e17062b2c8c8c95767b3b36b52054757136643
Likenesio <valentinubb@gmail.com>
2024-04-12 23:08:52 -0400

    se agrega carrosel a convenios

--- js/app.js
+++ js/app.js
@@ -1,28 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const burger = document.querySelector(".burger");
-    const navLinks = document.querySelector(".navbar-items");
-    const navLinksLi = document.querySelectorAll(".navbar-items li");
+  const burger = document.querySelector(".burger");
+  const navLinks = document.querySelector(".navbar-items");
+  const navLinksLi = document.querySelectorAll(".navbar-items li");
 
-    burger.addEventListener("click", () => {
-      navLinks.classList.toggle("nav-active");
+  burger.addEventListener("click", () => {
+    navLinks.classList.toggle("nav-active");
 
-      burger.classList.toggle("toggle");
-    });
+    burger.classList.toggle("toggle");
+  });
 
-    navLinksLi.forEach((link) => {
-      link.addEventListener("click", () => {
-        if (navLinks.classList.contains("nav-active")) {
-          navLinks.classList.remove("nav-active");
-          burger.classList.remove("toggle");
-        }
-      });
-    });
-    document.addEventListener("click", (e) => {
-      if (!burger.contains(e.target) && !navLinks.contains(e.target)) {
-        if (navLinks.classList.contains("nav-active")) {
-          navLinks.classList.remove("nav-active");
-          burger.classList.remove("toggle");
-        }
+  navLinksLi.forEach((link) => {
+    link.addEventListener("click", () => {
+      if (navLinks.classList.contains("nav-active")) {
+        navLinks.classList.remove("nav-active");
+        burger.classList.remove("toggle");
       }
     });
   });
+  document.addEventListener("click", (e) => {
+    if (!burger.contains(e.target) && !navLinks.contains(e.target)) {
+      if (navLinks.classList.contains("nav-active")) {
+        navLinks.classList.remove("nav-active");
+        burger.classList.remove("toggle");
+      }
+    }
+  });
+});
+let currentItem = 1;
+function moveCarousel(action, targetItem) {
+  const totalItems = 3;
+  if (action === "prev") {
+    currentItem = currentItem === 1 ? totalItems : currentItem - 1;
+  } else if (action === "next") {
+    currentItem = currentItem === totalItems ? 1 : currentItem + 1;
+  } else if (action === "goTo") {
+    currentItem = targetItem;
+  }
+  for (let i = 1; i <= totalItems; i++) {
+    document.getElementById(`itemCarrousel-${i}`).style.display = "none";
+  }
+  document.getElementById(`itemCarrousel-${currentItem}`).style.display =
+    "block";
+}
+setInterval(function () {
+  moveCarousel("next");
+}, 3000);
